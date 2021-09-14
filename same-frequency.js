177fdb0e7004a3cf20fcc114e8431ec59a8d5b30
Kateria Wynn <kwynn1211@gmail.com>
2021-09-13 23:06:30 -0700

    added numToStr, makeFreqObj, sameFrequency

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,36 @@
-// add whatever parameters you deem necessary
-function sameFrequency() {
+function numToStr(num) {
+  const str = num.toString();
+  return str;
 }
+
+function makeFreqObj(str1, str2, i = 0, freqObj1 = {}, freqObj2 = {}) {
+  // base case
+  if (str1.length === i) return { freqObj1, freqObj2 };
+
+  // interation
+  freqObj1[str1[i]] = freqObj1[str1[i]] + 1 || 1;
+  freqObj2[str2[i]] = freqObj2[str2[i]] + 1 || 1;
+
+  // return recursive func call
+  return makeFreqObj(str1, str2, i + 1, freqObj1, freqObj2);
+}
+
+function sameFrequency(num1, num2) {
+  const num1Str = numToStr(num1);
+  const num2Str = numToStr(num2);
+
+  if (num1Str.length !== num2Str.length) return false;
+
+  const { freqObj1, freqObj2 } = makeFreqObj(num1Str, num2Str);
+
+  for (const key in freqObj1) {
+    if (!(key in freqObj2)) {
+      return false;
+    } else if (freqObj1[key] === freqObj2[key]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+module.exports = sameFrequency;
