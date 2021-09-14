7833d8ce29d2aef0a9cea1c523abcfdd8d1bec4f
Kateria Wynn <kwynn1211@gmail.com>
2021-09-13 19:57:13 -0700

    added makeObj and twoArrayObject funcs

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,18 @@
-// add whatever parameters you deem necessary
-function twoArrayObject() {
+function makeObj(arrKeys, arrValues, i = 0, resultsObj = {}) {
+  // base case
+  // continue until the end of the arrKeys is reached
+  if (arrKeys.length === i) return resultsObj;
+
+  // iteration
+  resultsObj[arrKeys[i]] = arrValues[i] || null;
+
+  // return recurive call
+  return makeObj(arrKeys, arrValues, i + 1, resultsObj);
 }
+
+function twoArrayObject(arrKeys, arrValues) {
+  let resultsObj = makeObj(arrKeys, arrValues);
+  return resultsObj;
+}
+
+module.exports = twoArrayObject;
